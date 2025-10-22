1d8da4eecdf9278cc7ca48eb4366a4f56452d87f
Swan <swanand556@gmail.com>
2025-10-22 22:59:03 +0530

    backend done

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,9 @@
+from fastapi import FastAPI
+from backend.src.routers import notes
+from backend.src.models import Base
+from backend.src.database import engine
 
 app = FastAPI()
+Base.metadata.create_all(engine)
 
-app.add_route()
+app.include_router(notes.router)
