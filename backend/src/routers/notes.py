efc249736c3b8c567f1c728b78ff4170b272fa56
Swan <swanand556@gmail.com>
2025-10-22 22:53:24 +0530

    completed router functions

--- backend/src/routers/notes.py
+++ backend/src/routers/notes.py
@@ -9,10 +9,31 @@ router = APIRouter(
     prefix="/note"
 )
 
-@router.post("/", response_model=Note)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def createNote(request:Note, db: Session=Depends(get_db)):
-    new_note = models.Note(header=request.header, body=request.body, tags=request.tags)
+    new_note = models.Note(header=request.header, body=request.body, tags=request.tags) # type: ignore
     db.add(new_note)
     db.commit()
     db.refresh(new_note)
     return new_note
+
+@router.put("/{id}", status_code=status.HTTP_200_OK)
+def loadNote(id: int, db:Session=Depends(get_db)):
+    note = db.query(models.Note).filter(models.Note.id==id).first()
+
+    if not note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"note with id={id} note found"
+        )
+    return note
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def removeNote(id: int, db:Session=Depends(get_db)):
+    note = db.query(models.Note).filter(models.Note.id==id).first()
+    if not note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"note with id={id} not found"
+        )
+    return note
