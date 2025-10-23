6e9d015306f05c557d7fa1c3cf4f4c94fc7eb65b
Swan <swanand556@gmail.com>
2025-10-23 15:31:51 +0530

    draggable windows created

---
+++ frontend/src/routes/Note.svelte
@@ -0,0 +1,55 @@
+<script>
+		import Draggable from './Draggable.svelte';
+</script>
+
+<Draggable>
+	<div class="note">
+        <div class="header">
+            <input type="bello" class="headerInput">
+        </div>
+        <div class="body">
+            <textarea class="bodyInput" on:mousedown|stopPropagation
+  on:touchstart|stopPropagation></textarea>
+        </div>
+    </div>
+</Draggable>
+
+<style>
+    .note {
+        min-width: 200px;
+        
+    }
+    .headerInput {
+        height: 90%;
+        min-width: 70%;
+        background: transparent;
+        border: 0px solid;
+        margin-left: 5px;
+    }
+    .headerInput:focus {
+        outline: none;
+        border: 0px solid;
+    }
+
+    .header {
+        height: 30px;
+        background-color: yellow;
+    }
+    .body {
+        background-color: yellowgreen;
+        min-height: 170px;
+    }
+
+    .bodyInput {
+        background-color: transparent;
+        border: 0px solid;
+        min-width: 195px;
+        min-height: 170px;
+        margin-left: 5px;
+    }
+
+    .bodyInput:focus {
+        border: 0px solid;
+        outline: none;
+    }
+</style>
